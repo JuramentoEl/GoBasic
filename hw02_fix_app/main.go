4707454d583d5845cf05fbd594111b862d2e4fdc
JuramentoEl <liza.bolonina@yandex.ru>
2024-09-01 13:41:31 +0300

    fix lint

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"hw02_fix_app/printer"
+	"hw02_fix_app/reader"
+	"hw02_fix_app/types"
 )
 
 func main() {
-	var path string = "data.json"
+	var path string
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -19,10 +19,9 @@ func main() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	staff, err = reader.ReadJSON(path)
 
 	fmt.Print(err)
 

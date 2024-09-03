05f026cd8da2deed29ca911bdeccabb9e95a10b1
JuramentoEl <liza.bolonina@yandex.ru>
2024-09-03 20:58:07 +0300

    chessboard

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	var size int
+
+	fmt.Printf("Enter data file path: ")
+	fmt.Scanln(&size)
+
+	if size == 0 {
+		size = 8
+	}
+
+	for i := 1; i <= size; i++ {
+		for j := 1; j <= size; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
 }

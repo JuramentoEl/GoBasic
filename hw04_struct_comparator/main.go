11e033bc93f2bb114ee96b0f38a042f285dd36f1
JuramentoEl <liza.bolonina@yandex.ru>
2024-09-17 21:50:27 +0300

    v_4

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 )
 
-type Book struct {
-	ID     int
-	Title  string
-	Author string
-	Year   int
-	Size   int
-	Rate   float32
+type book struct {
+	ID              int
+	Title           string
+	Author          string
+	Year            int
+	Size            int
+	Rate            float32
+	FiledComparison indexField
 }
 
 type indexField int
@@ -21,8 +22,8 @@ const (
 	Rate
 )
 
-func (b *Book) saveBook(id int, title string, author string, year int, size int, rate float32) Book {
-	book := Book{
+func (b *book) setBook(id int, title string, author string, year int, size int, rate float32) book {
+	book := book{
 		ID:     id,
 		Title:  title,
 		Author: author,
@@ -34,7 +35,7 @@ func (b *Book) saveBook(id int, title string, author string, year int, size int,
 	return book
 }
 
-func (b *Book) getBook() bool {
+func (b *book) getBook() bool {
 	fmt.Println("ID: ", b.ID)
 	fmt.Println("Title: ", b.Title)
 	fmt.Println("Author: ", b.Author)
@@ -45,16 +46,24 @@ func (b *Book) getBook() bool {
 	return true
 }
 
+func (b *book) setFiledComparison(FiledComparison indexField) {
+	b.FiledComparison = FiledComparison
+}
+
+func (b *book) getFiledComparison() indexField {
+	return b.FiledComparison
+}
+
 func main() {
-	books := make(map[int]Book)
+	books := make(map[int]book)
 	count := 0
 
 	actionSelection(&count, books)
 }
 
-func actionSelection(count *int, books map[int]Book) {
+func actionSelection(count *int, books map[int]book) {
 	var action int
-	var book Book
+	var book2, book1 book
 
 	fmt.Println("1: сохранить книгу")
 	fmt.Println("2: получить данные о книге")
@@ -66,33 +75,37 @@ func actionSelection(count *int, books map[int]Book) {
 	case 1:
 		*count++
 		books[*count] = scanBook(*count)
-		book = books[*count]
+		book2 = books[*count]
 		actionSelection(count, books)
 	case 2:
 		var IDBook int
 		fmt.Print("В базе ", *count, " книг. Выберете одну: ")
 		fmt.Scanln(&IDBook)
-		book = books[IDBook]
-		book.getBook()
+		book2 = books[IDBook]
+		book2.getBook()
 		actionSelection(count, books)
 	case 3:
-		var book1, book2 int
+		var bookIndex1, bookIndex2 int
 		var filed indexField
 		var result bool
 		fmt.Print("В базе ", *count, " книг. ")
 		fmt.Println("Введите номера книг для сранение и имя поля через пробел (1 - year, 2 - size или  3 - rate)")
-		fmt.Scanln(&book1, &book2, &filed)
-		result = comparisonBooks(books[book1], books[book2], filed)
+		fmt.Scanln(&bookIndex1, &bookIndex2, &filed)
+		book2 = books[bookIndex1]
+		book2.setFiledComparison(filed)
+		book1 = books[bookIndex2]
+		book1.setFiledComparison(filed)
+		result = comparisonBooks(book2, book1)
 		fmt.Println(result)
 		actionSelection(count, books)
 	}
 }
 
-func scanBook(id int) Book {
+func scanBook(id int) book {
 	var title, author string
 	var year, size int
 	var rate float32
-	var book Book
+	var book book
 
 	fmt.Println("Ввод данных книги")
 	fmt.Print("Название: ")
@@ -105,13 +118,13 @@ func scanBook(id int) Book {
 	fmt.Scanln(&size)
 	fmt.Print("Редкость: ")
 	fmt.Scanln(&rate)
-	book = book.saveBook(id, title, author, year, size, rate)
+	book = book.setBook(id, title, author, year, size, rate)
 
 	return book
 }
 
-func comparisonBooks(book1 Book, book2 Book, filed indexField) bool {
-	switch filed {
+func comparisonBooks(book1 book, book2 book) bool {
+	switch book2.getFiledComparison() {
 	case Year:
 		return book1.Year > book2.Year
 	case Size:
@@ -119,6 +132,7 @@ func comparisonBooks(book1 Book, book2 Book, filed indexField) bool {
 	case Rate:
 		return book1.Rate > book2.Rate
 	}
-
+	fmt.Println("book1: ", book1.FiledComparison)
+	fmt.Println("book2: ", book2.FiledComparison)
 	return false
 }

bea065c7059f4ec2b13cae99f720ac1fafd66bda
JuramentoEl <liza.bolonina@yandex.ru>
2024-10-20 20:48:52 +0300

    v1

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+	text := "зима осень  зима лето осень лето зима весна"
+	result := CountWords(text)
+	fmt.Println(result)
+}
+
+func CountWords(s string) map[string]int {
+
+	var LowerWord string
+	result := make(map[string]int)
+	fmt.Println(s)
+	//words := strings.Fields(s)
+	words := strings.Split(s, " ")
+	fmt.Println(words)
+	for _, word := range words {
+		if word != "" {
+			LowerWord = strings.ToLower(word)
+			result[LowerWord]++
+		}
+	}
+
+	return result
 }

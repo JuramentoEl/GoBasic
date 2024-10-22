116bc23a8f72d6c9ee5584efe0656899cfc03c65
JuramentoEl <liza.bolonina@yandex.ru>
2024-10-22 10:52:39 +0300

    v2

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -15,14 +15,10 @@ func CountWords(s string) map[string]int {
 	var LowerWord string
 
 	result := make(map[string]int)
-	fmt.Println(s)
-	words := strings.Split(s, " ")
-	fmt.Println(words)
+	words := strings.Fields(s)
 	for _, word := range words {
-		if word != "" {
-			LowerWord = strings.ToLower(word)
-			result[LowerWord]++
-		}
+		LowerWord = strings.ToLower(word)
+		result[LowerWord]++
 	}
 	return result
 }

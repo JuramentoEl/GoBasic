1d292e1c5e0b61ebe658983fd59c20ecf6bc92c3
JuramentoEl <liza.bolonina@yandex.ru>
2024-12-03 20:11:14 +0300

    v1

--- hw13_http/main.go
+++ hw13_http/main.go
@@ -1,5 +1,27 @@
 package main
 
+type Book struct {
+	id     int
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float32
+}
+
+func NewBook(id int, title, author string, year, size int, rate float32) *Book {
+	book := Book{
+		id:     id,
+		title:  title,
+		author: author,
+		year:   year,
+		size:   size,
+		rate:   rate,
+	}
+
+	return &book
+}
+
 func main() {
-	// Place your code here.
+
 }
